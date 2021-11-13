3c29d376fbbde1fbffbb0d74dbf325dbfabd52df
Jordan Roberson <jordanrobersonnp@gmail.com>
2021-11-13 06:28:28 -0500

    added tags to quotes, adjusted css

--- js/script.js
+++ js/script.js
@@ -15,48 +15,58 @@ let quotes = [
     quote: "Stay hungry. Stay Foolish",
     source: "Steve Jobs",
     year: 2008,
+    tags: "Business",
   },
   {
     quote:
       "Some are born great, some achieve greatness, and some have greatness thrust upon them.",
     source: "William Shakespeare",
     citation: "Twelfth Night",
+    tags: "Theater",
   },
   {
     quote:
       "We build our computer systems the way we build our cities: over time, without a plan, on top of ruins.",
     source: "Ellen Ullman",
+    tags: "Programming",
   },
   {
     quote:
       "One of the best programming skills you can have is knowing when to walk away for a while.",
     source: "Oscar Godson",
+    tags: "Programming",
   },
   {
     quote: "The computer was born to solve problems that did not exist before",
     source: "Bill Gates",
+    tags: "Programming",
   },
   {
     quote:
       "Any fool can write code that a computer can understand. Good programmers write code that humans can understand",
     source: "Martin Fowler",
+    tags: "Programming",
   },
   {
     quote: "First, solve the problem. Then, write the code.",
     source: "John Johnson",
+    tags: "Programming",
   },
   {
     quote: "Knowledge is power.",
     source: "Francis Bacon",
+    tags: "Business",
   },
   {
     quote: "Code is like humor. When you have to explain it, it’s bad.",
     source: "Cory House",
+    tags: "Programming",
   },
   {
     quote:
       "In some ways, programming is like painting. You start with a blank canvas and certain basic raw materials. You use a combination of science, art, and craft to determine what to do with them.",
     source: "Andrew Hunt",
+    tags: "Programming",
   },
 ];
 
@@ -81,6 +91,10 @@ function printQuote() {
 
   if (randomQuote.year) {
     printedQuote += `<span class="year">${randomQuote.year}</span>`;
+  }
+
+  if (randomQuote.tags) {
+    printedQuote += `<span class="tags">${randomQuote.tags}</span>`;
   }
 
   printedQuote += "</p>";
